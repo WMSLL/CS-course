c74d1b2db69eb0134df1d83d6c6f8a1fb3e5abb2
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-03-16 21:33:19 +0300

    Создали

---
+++ LESSONS/2/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int 
+            //int number1=33;
+            //Console.WriteLine(number1);
+            //
+            //
+            //short Short1 = 4432;
+            //Console.WriteLine(Short1);
+            //
+            //
+            //float Float1 = 23.3354f;
+            //Console.WriteLine(Float1);
+            //
+            //
+            //
+            //
+            //decimal Dec12 = 3243.423m;
+            //Console.WriteLine(Dec12);
+            //
+            //string line = Console.ReadLine();
+            //
+            //decimal number = decimal.Parse(line);  // Parse есть у всех типов
+            //Console.WriteLine(number);
+            //
+            //Console.WriteLine(Math.Pow(2, 10));
+            //
+            //bool b = true;
+            //bool b2 = false;
+
+
+            //Console.WriteLine(bool.Parse("tre"));
+            Console.WriteLine(!(5>=10));
+            Console.WriteLine((5 >= 10));
+            Console.WriteLine("dd"=="dd");
+            Console.WriteLine("dd" != "dod");
+
+            // char b = '\u1234'; // Номер какого нить символа нпример смайла
+
+            // Console.WriteLine(b);
+
+
+        }
+    }
+}
